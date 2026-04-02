e255e6445f250047bb329cfab574a6c9da78b6dc
Hugo Gabriel Pereira de Almeida <hugogpdealmeida@gmail.com>
2026-04-02 11:50:49 +0200

    Add links to layouts

--- app/src/main/java/com/example/imc/ComputeActivity.java
+++ app/src/main/java/com/example/imc/ComputeActivity.java
@@ -1,4 +1,12 @@
 package com.example.imc;
 
-public class ComputeActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ComputeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compute);
+    }
 }
--- app/src/main/java/com/example/imc/CustomActivity.java
+++ app/src/main/java/com/example/imc/CustomActivity.java
@@ -1,4 +1,12 @@
 package com.example.imc;
 
-public class CustomActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom);
+    }
 }
